91d8652d3092786a4007a45c8bbf51ef557ae65e
Justin Tadlock <justintadlock@gmail.com>
2022-03-15 14:31:36 -0500

    Update `$pagination` call.

--- public/views/parts/pagination.php
+++ public/views/parts/pagination.php
@@ -1,8 +1,8 @@
-<?php $pagination->make( [
+<?php $pagination->display( [
 	'prev_text'       => '&larr; Previous',
 	'next_text'       => 'Next &rarr;',
 	'container_class' => 'pagination clear mt-8 text-center',
 	'list_class'      => 'pagination__items list-none inline-block m-0 p-0',
 	'item_class'      => 'pagination__item pagination__item--%s inline-block text-base font-600',
 	'anchor_class'    => 'pagination__anchor pagination__anchor--%s block py-3 px-6 border border-gray-200 no-underline',
-] )->display() ?>
+] ) ?>
