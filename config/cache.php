ad953f23a3e793a860f44141207a6efe9b04df94
Justin Tadlock <justintadlock@gmail.com>
2022-03-22 02:26:20 -0500

    Upgrade to latest `dev-master` of framework and add the new `expires` key for the cache config.

--- config/cache.php
+++ config/cache.php
@@ -8,6 +8,16 @@ return [
 	// yoursite.com/purge/cache/global/{$key}
 	'purge_key' => env( 'PURGE_KEY' ),
 
+	// Set a default cache expiration time. This should be a time in seconds.
+	// If set to 0, cached data will remain until it is purged or the system
+	// triggers a flush. Individual cache implementations may override this
+	// expiration date. You may use one of the following constants provided
+	// by the framework for quickly adding a time:
+	//
+	// MINUTE_IN_SECONDS | HOUR_IN_SECONDS | DAY_IN_SECONDS
+	// WEEK_IN_SECONDS  | MONTH_IN_SECONDS | YEAR_IN_SECONDS
+	'expires' => 0,
+
 	// Exclude metadata (frontmatter) from being cached. This will keep
 	// cached file sizes smaller. Note that meta used for sorting queries,
 	// such as `title`, `date`, and taxonomy content types, should be
