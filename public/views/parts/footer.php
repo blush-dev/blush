cbae598518301b041332c2dd15c80fc1ab204437
Justin Tadlock <justintadlock@gmail.com>
2022-03-22 22:53:04 -0500

    Activate superpowers.

--- public/views/parts/footer.php
+++ public/views/parts/footer.php
@@ -1,6 +1,6 @@
 	<footer class="app-footer py-8 font-secondary text-center text-xl border-b-2 border-blue-200">
 		<p class="app-footer__credit text-gray-600">
-			Powered by heart and soul.
+			<?= Blush\PoweredBy::render() ?>
 		</p>
 
 		<div class="menu menu--social mt-2">
